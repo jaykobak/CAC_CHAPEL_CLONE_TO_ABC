b26371515e1fdf3596cabfe51eb5493515829267
Urkel <urkelcodes7@gmail.com>
2024-10-26 11:38:22 +0100

    slit changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import About from "./pages/About"
-import Home from "./pages/Home"
+import About from "@/pages/About"
+import Home from "@/pages/Home"
 
 function App() {
 
