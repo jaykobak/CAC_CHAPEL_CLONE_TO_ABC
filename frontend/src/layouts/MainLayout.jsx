86f048be2db43deecd3e566618dc9b804c448cff
Urkel <urkelcodes7@gmail.com>
2024-11-12 15:53:53 +0100

    new changes

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <div className='bg-background min-h-screen font-inter max-w-[1500px]'>
-      <Navbar />
-      <div className='pb-10'>
-        {children}
+    <div className='bg-background min-h-screen font-inter  flex justify-center'>
+      <div className='w-full max-w-[1900px]'>
+        <Navbar />
+        <div className='pb-10 w-full'>
+          {children}
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }
