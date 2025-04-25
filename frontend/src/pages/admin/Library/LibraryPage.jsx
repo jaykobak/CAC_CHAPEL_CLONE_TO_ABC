9c9e4dafb7bdf8296593548d254bc71bac6af461
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-04-25 14:19:34 +0100

    cool one and nice cool

---
+++ frontend/src/pages/admin/Library/LibraryPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AdminLayout from '@/layouts/AdminLayout'
+import AdminPadding from '@/layouts/AdminPadding'
+
+const AdminLibraryPage = () => {
+    return (
+        <AdminLayout>
+            <AdminPadding>
+
+            </AdminPadding>
+        </AdminLayout>
+    )
+}
+
+export default AdminLibraryPage;
