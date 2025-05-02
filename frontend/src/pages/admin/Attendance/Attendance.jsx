5e08114e28e5c9b74ea012791be13ede1af17879
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-05-02 12:20:03 +0100

    ire deh ire mama deh

--- frontend/src/pages/admin/Attendance/Attendance.jsx
+++ frontend/src/pages/admin/Attendance/Attendance.jsx
@@ -1,5 +1,3 @@
-import List from '@/components/Admin/Attendance/List/List'
-import Navbar from '@/components/Admin/Attendance/Navbar/Navbar'
 import AdminLayout from '@/layouts/AdminLayout'
 import AdminPadding from '@/layouts/AdminPadding'
 import React from 'react'
@@ -7,12 +5,10 @@ import React from 'react'
 const Attendance = () => {
   return (
     <AdminLayout>
-        <Navbar />
-        <AdminPadding>
-          <List />
-        </AdminPadding>
+      <AdminPadding>
+      </AdminPadding>
     </AdminLayout>
-  ) 
+  )
 }
 
 export default Attendance
